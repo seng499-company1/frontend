b70e0ead08d5f32504eedb705e81a32c37770985
Noah Clarke <noahwclarke@uvic.ca>
2022-05-31 20:35:08 -0700

    cleanout example story

--- best-uvic-scheduler/.storybook/main.js
+++ best-uvic-scheduler/.storybook/main.js
@@ -1,16 +1,40 @@
+// Thanks @saasquatch for the storybook config
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+  stories: ["../src/**/*.stories.@(js|jsx|ts|tsx|mdx)"],
+  addons: [
+    {
+      name: "@storybook/addon-docs",
+      options: {
+        configureJSX: true,
+        babelOptions: {},
+        sourceLoaderOptions: null,
+        transcludeMarkdown: true,
+      },
+    },
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@storybook/preset-create-react-app"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-webpack5"
-  }
-}
+  babel: async (options) => ({
+    ...options,
+
+    plugins: [
+      "@babel/plugin-proposal-optional-chaining",
+      "@babel/plugin-transform-runtime",
+    ],
+  }),
+  webpackFinal: async (config) => {
+    config.module.rules[0].use[0].options.plugins[1] = [
+      "@babel/plugin-proposal-class-properties",
+      { loose: true },
+    ];
+
+    config.module.rules.push({
+      test: /\.(stories|story)\.[tj]sx?$/,
+      loader: require.resolve("@storybook/source-loader"),
+      exclude: [/node_modules/],
+      enforce: "pre",
+    });
+
+    config.resolve.extensions.push(".ts", ".tsx", ".mdx");
+
+    return config;
+  },
+};
