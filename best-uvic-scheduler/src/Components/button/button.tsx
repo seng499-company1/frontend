58f40ce0d2d0be6d445e65c610d0b008784185b0
RileyJRaso <riley.j.raso@gmail.com>
2022-06-05 20:57:19 -0700

    Changed some button colours to look nicer :)

--- best-uvic-scheduler/src/Components/button/button.tsx
+++ best-uvic-scheduler/src/Components/button/button.tsx
@@ -9,9 +9,9 @@ const ButtonTheme = {
     textcolor: "#FEFEFE"
   },
   Secondary: {
-    default: "#B8B5B4",
-    hover: "#A09E9E",
-    textcolor: "#000000"
+    default: "#EEEEEE",
+    hover: "#DEDEDE",
+    textcolor: "#5383F9"
   },
   Cancel: {
     default: "#F95353",
@@ -26,15 +26,18 @@ export interface CustomButtonProps {
   children: React.ReactNode;
 }
 
+//
 const CustomButton = styled.button`
   background-color: ${(props) => ButtonTheme[props.Theme].default};
   color: ${(props) => ButtonTheme[props.Theme].textcolor};
-  box-shadow: 0px 2px 2px lightgray;
   cursor: pointer;
+  box-shadow: 0px 2px 2px lightgray;
+  border: 2px solid ${(props) => ButtonTheme[props.Theme].default};
   border-radius: 7px;
   padding: 5px 15px;
   &:hover {
     background-color: ${(props) => props.Disabled ? ButtonTheme[props.Theme].default : ButtonTheme[props.Theme].hover};
+    border: 2px solid ${(props) => props.Disabled ? ButtonTheme[props.Theme].default : ButtonTheme[props.Theme].hover};
   }
   ${({ Disabled }) => Disabled && `
     cursor: default;
