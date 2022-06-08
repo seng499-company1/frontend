8ae2f8afca59235c963849c58a9523158c4a817c
RileyJRaso <riley.j.raso@gmail.com>
2022-06-07 17:06:40 -0700

    Final Search Design

--- best-uvic-scheduler/src/Components/SearchBar/Searchbar.tsx
+++ best-uvic-scheduler/src/Components/SearchBar/Searchbar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import '../../index.css';
-
+import { BiSearchAlt2 } from "react-icons/bi";
 
 export interface SearchBarProps {
   children: React.ReactNode;
@@ -19,6 +19,7 @@ const SearchBarDiv = styled.div`
   margin: 0;
   padding: 0;
   display: flex;
+  align-items: center;
   justifyContent: start;
   background-color: #D9D9D9;
   border: 2px solid #000000;
@@ -27,12 +28,14 @@ const SearchBarDiv = styled.div`
 
 const SearchBarInput = styled.input`
   padding: 0.5em;
-  margin: 0 0.5em 0 0.5em;
+  margin: 0 0.2em 0 0.2em;
   width: 50%;
   background-color: #D9D9D9;
   color: #000000;
   font-weight: bold;
   border: 2px solid #C9C9C9;
+  border-top-width: 0;
+  border-bottom-width: 0;
   border-radius: 4px;
 `;
 
@@ -51,6 +54,7 @@ export function SearchBarDivView(props: SearchBarDivProps) {
 export function SearchBarView(props: SearchBarProps) {
   return (
     <SearchBarDivView>
+      <BiSearchAlt2 style={{ height: 30, width: 30 }}/>
       <SearchBarInputView/>
     </SearchBarDivView>
   );
