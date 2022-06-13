d8878abc4a8242b6ccd00d10f7c55f2fadf53f98
Shayla Grymaloski <shaylagrymaloski@Shaylas-MacBook-Pro-2.local>
2022-06-12 20:55:11 -0700

    Pr suggestions

--- best-uvic-scheduler/src/index.js
+++ best-uvic-scheduler/src/index.js
@@ -5,16 +5,15 @@ import "./index.css";
 import App from "./App";
 import PDISelectProfessorList from "./Pages/ProfessorDataInput/ProfessorDataInput_SelectProfessorList.tsx";
 import Summary from "./Pages/Confirmation/Summary.tsx";
-// import Summary from "./Pages/Confirmation/SummaryPage";
 import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/SelectProfessor" element={<PDISelectProfessorList />} />
+        <Route path="/SelectProfessor/*" element={<PDISelectProfessorList />} />
         <Route path="/Summary" element={<Summary />} />
       </Routes>
     </BrowserRouter>
