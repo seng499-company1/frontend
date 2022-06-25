2198ee0459cf60e4e58871fe3a0183467a740a34
Shayla Grymaloski <shaylagrymaloski@Shaylas-MacBook-Pro-2.local>
2022-06-24 21:28:09 -0700

    first draft of landing page

--- best-uvic-scheduler/src/Components/Alert/alert.tsx
+++ best-uvic-scheduler/src/Components/Alert/alert.tsx
@@ -7,42 +7,42 @@ export interface AlertProps {
 }
 
 const RedBox = styled.div`
+  font-size: 0;
+  width: 500px;
+  border-radius: 10px;
+  box-shadow: 1px 1px;
   display: grid;
-  height: 48px;
-  background-color: var(--danger-700);
-  border-style: solid;
-  opacity: 0.75;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 32px;
+  background-color: var(--danger-600);
   grid-template-columns: repeat(3, 1fr);
-  box-shadow: 0.5px 1px;
 `;
 
 const AlertText = styled.p`
   text-shadow: 0.25px 0.5px black;
-  display: flex;
   opacity: 1;
-  font-weight: bold;
   grid-column: 1 / 3;
   color: black;
-  font-size: 24px;
+  font-size: 16px;
   grid-row: 1;
   font-style: bold;
-  padding: 0px 0px 0px 48px;
 `;
 
 const Message = styled.p`
   color: black;
   grid-column: 2 / 3;
-  text-align: center;
-  font-size: 16px;
+  font-size: 12px;
   font-style: italic;
+  font-style: bold;
   grid-row: 1;
-  padding: 14px 0px 0px 0px;
 `;
 
 export function Alert(props: AlertProps) {
   return (
     <RedBox>
-      <AlertText>Alert:</AlertText>
+      <AlertText>Alert:&emsp;&emsp;&emsp;</AlertText>
       <Message>
         *You have {props.new_entries} new professor data entries*
       </Message>
