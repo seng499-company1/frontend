a1aafe7f10e8a613f15c2a57b5215dd9372be495
Aleah <aleah4@me.com>
2022-06-26 17:50:25 -0700

    changed course list helper logic

--- best-uvic-scheduler/src/Util/CourseListHelper.tsx
+++ best-uvic-scheduler/src/Util/CourseListHelper.tsx
@@ -1,45 +1,63 @@
-export function GetCourseList() {
-  // const tenant = "localhost:5000";
-  // const resp = await fetch(`http://${tenant}/professors`, {
-  //   method: "GET",
-  // });
+const TestCourseList = {};
 
-  // const data = await resp.text();
-  // console.log(data);
-  //return resp.json;
+//change to async!!
+export async function GetCourseList() {
+  const tenant = "localhost:5000";
+  const resp = await fetch(`http://${tenant}/courses`, {
+    method: "GET",
+  });
 
+  const data = await resp.text();
+  console.log(data);
+
+  if (Courses in TestCourseList) {
+    TestCourseList["Courses"].push(resp.json);
+  } else {
+    TestCourseList["Courses"] = [resp.json];
+  }
   return TestCourseList;
 }
 
-const TestCourseList = {
-  Courses: [
-    {
-      uuid: "1e90ab30-c380-4034-acdb-238856a88df3",
-      course_code: "CSC 111",
-      course_name: "Fundamentals of C Programming",
-      course_desceiption:
-        "A class to teach the fundamentals of programming for new students",
-      course_qualifications: "Strong fundamentals",
-      peng_req: true,
-    },
-    {
-      uuid: "3e90ab30-c380-4034-acdb-238856a88df3",
-      course_code: "CSC 230",
-      course_name: "Assembly ARM programming",
-      course_desceiption:
-        "Starting to program with a physical system and learning about assemby",
-      course_qualifications: "3 years of industry experience",
-      peng_req: false,
-    },
-    {
-      uuid: "2e90ab30-c380-4034-acdb-238856a88df3",
-      course_code: "SENG 275",
-      course_name: "Software Testing",
-      course_desceiption:
-        "learning the fundamentals of testing and different frameworks to use",
-      course_qualifications:
-        "worked on projects with team sizes of over 5 people",
-      peng_req: true,
-    },
-  ],
-};
+// export async function test() {
+//   const tenant = "localhost:5000";
+//   const resp = await fetch(`http://${tenant}/courses`, {
+//     method: "GET",
+//   });
+
+//   const data = await resp.text();
+//   console.log(data);
+//   return resp.json;
+// }
+
+// const TestCourseList = {
+//   Courses: [
+//     {
+//       uuid: "1e90ab30-c380-4034-acdb-238856a88df3",
+//       course_code: "CSC 111",
+//       course_name: "Fundamentals of C Programming",
+//       course_desceiption:
+//         "A class to teach the fundamentals of programming for new students",
+//       course_qualifications: "Strong fundamentals",
+//       peng_req: true,
+//     },
+//     {
+//       uuid: "3e90ab30-c380-4034-acdb-238856a88df3",
+//       course_code: "CSC 230",
+//       course_name: "Assembly ARM programming",
+//       course_desceiption:
+//         "Starting to program with a physical system and learning about assemby",
+//       course_qualifications: "3 years of industry experience",
+//       peng_req: false,
+//     },
+//     {
+//       uuid: "2e90ab30-c380-4034-acdb-238856a88df3",
+//       course_code: "SENG 275",
+//       course_name: "Software Testing",
+//       course_desceiption:
+//         "learning the fundamentals of testing and different frameworks to use",
+//       course_qualifications:
+//         "worked on projects with team sizes of over 5 people",
+//       peng_req: true,
+//     },
+//   ],
+// };
