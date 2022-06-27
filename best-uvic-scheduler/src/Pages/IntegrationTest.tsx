adfae345402e76a984de62e7f53e0404b7c25bb3
Noah Clarke <noahwclarke@uvic.ca>
2022-06-26 17:32:43 -0700

    start integration test hook

--- best-uvic-scheduler/src/Pages/IntegrationTest.tsx
+++ best-uvic-scheduler/src/Pages/IntegrationTest.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import CustomButtonView from "../Components/button/button.tsx";
 import { Background } from "../Components/background/background.tsx";
 import TabGroup from "../Components/tab-group/tab-group.tsx";
 
 interface IntegrationTestProps {}
-interface IntegrationTestViewProps {}
+interface IntegrationTestViewProps {
+  company1Output: any;
+  company2Output: any;
+  onCompany1Request: any;
+  onCompany2Request: any;
+  company: number;
+  setCompany: React.Dispatch<React.SetStateAction<number>>;
+}
 
 function useIntegrationTest(props: IntegrationTestProps) {
-  return {};
+  const [company, setCompany] = useState(1);
+
+  return {
+    company,
+    setCompany,
+    company1Output: "",
+    company2Output: "",
+    onCompany1Request: () => console.log("click 2"),
+    onCompany2Request: () => console.log("click 2"),
+  };
 }
 
 const ButtonDiv = styled.div`
@@ -22,27 +38,58 @@ const PageTitleH1 = styled.h1`
   text-align: center;
 `;
 
+const EmptyP = styled.p`
+  padding: var(--space-3x-large);
+  text-align: center;
+`;
+
 export function IntegrationTestView(props: IntegrationTestViewProps) {
-  const {} = props;
+  const {
+    company,
+    setCompany,
+    company1Output,
+    company2Output,
+    onCompany1Request,
+    onCompany2Request,
+  } = props;
   return (
     <Background>
       <PageTitleH1>Integration test</PageTitleH1>
       <ButtonDiv>
-        <CustomButtonView {...{ Theme: "Primary" }}>
+        <CustomButtonView
+          {...{ Theme: "Primary", customClickEvent: () => onCompany1Request() }}
+        >
           Run Company 1 Algorithm
         </CustomButtonView>
-        <CustomButtonView {...{ Theme: "Primary" }}>
+        <CustomButtonView
+          {...{ Theme: "Primary", customClickEvent: () => onCompany2Request() }}
+        >
           Run Company 2 Algorithm
         </CustomButtonView>
       </ButtonDiv>
       <TabGroup initialTabId="1">
-        <TabGroup.Tab size="medium" tabId="1">
+        <TabGroup.Tab size="medium" tabId="1" onClick={() => setCompany(1)}>
           Company 1 Output
         </TabGroup.Tab>
-        <TabGroup.Tab size="medium" tabId="2">
+        <TabGroup.Tab size="medium" tabId="2" onClick={() => setCompany(2)}>
           Company 2 Output
         </TabGroup.Tab>
       </TabGroup>
+      {company === 1 ? (
+        company1Output ? (
+          <code>{company1Output}</code>
+        ) : (
+          <EmptyP>
+            No output from Company 1 yet. Click the run button to get output!
+          </EmptyP>
+        )
+      ) : company2Output ? (
+        <code>{company2Output}</code>
+      ) : (
+        <EmptyP>
+          No output from Company 2 yet. Click the run button to get output!
+        </EmptyP>
+      )}
     </Background>
   );
 }
