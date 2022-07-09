2b275770bb08d9734f50713d6ee8f913f939011a
Aleah <aleah4@me.com>
2022-07-09 16:26:36 -0700

    hot fix: change new server link

--- best-uvic-scheduler/src/Util/CourseListHelper.tsx
+++ best-uvic-scheduler/src/Util/CourseListHelper.tsx
@@ -5,7 +5,7 @@ const axios = require("axios");
 export async function GetCourseList() {
   try {
     const response = await axios.get(
-      "https://com-one-tester.herokuapp.com/courses/"
+      "http://uvic.immortalmind.ca:5000/courses/"
     );
     console.log("response  ", response);
     return response.data;
