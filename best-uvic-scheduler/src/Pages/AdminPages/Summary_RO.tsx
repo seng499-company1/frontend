4382da6dddc2010cbcea03c51acd606f1967d3dd
Shayla Grymaloski <shaylagrymaloski@w87-173-047.wireless.uvic.ca>
2022-07-11 11:21:50 -0700

    Hotfix - fixed weekday label for summaryRO

--- best-uvic-scheduler/src/Pages/AdminPages/Summary_RO.tsx
+++ best-uvic-scheduler/src/Pages/AdminPages/Summary_RO.tsx
@@ -333,7 +333,7 @@ export function Summary_RO() {
                         <SelectableTableLabelsView></SelectableTableLabelsView>
                         <SelectableTableLabelsView>
                           {" "}
-                          {Day}
+                          {day}
                         </SelectableTableLabelsView>
                         <SelectableTableLabelsView>
                           {" "}
