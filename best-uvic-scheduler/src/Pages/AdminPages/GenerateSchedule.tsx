5dbb10eeb26d72357a352beb62227c95c7d5bced
Noah Clarke <noahwclarke@gmail.com>
2022-07-26 02:17:31 -0700

    loading spinner

--- best-uvic-scheduler/src/Pages/AdminPages/GenerateSchedule.tsx
+++ best-uvic-scheduler/src/Pages/AdminPages/GenerateSchedule.tsx
@@ -167,6 +167,49 @@ const RedBox = styled.div`
   margin: 48px auto 0;
 `;
 
+const LoadingSpinner = styled.div`
+  margin: auto;
+  text-align: center;
+  padding: 96px;
+  & {
+    .lds-ring {
+      display: inline-block;
+      position: relative;
+      width: 80px;
+      height: 80px;
+    }
+    .lds-ring div {
+      box-sizing: border-box;
+      display: block;
+      position: absolute;
+      width: 64px;
+      height: 64px;
+      margin: 8px;
+      border: 8px solid var(--primary);
+      border-radius: 50%;
+      animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+      border-color: var(--primary) transparent transparent transparent;
+    }
+    .lds-ring div:nth-child(1) {
+      animation-delay: -0.45s;
+    }
+    .lds-ring div:nth-child(2) {
+      animation-delay: -0.3s;
+    }
+    .lds-ring div:nth-child(3) {
+      animation-delay: -0.15s;
+    }
+    @keyframes lds-ring {
+      0% {
+        transform: rotate(0deg);
+      }
+      100% {
+        transform: rotate(360deg);
+      }
+    }
+  }
+`;
+
 function CreateListelement(scheduleElement) {
   const couseID = scheduleElement.course.code;
 
@@ -364,7 +407,16 @@ export function GenerateScheduleView(props: GenerateScheduleViewProps) {
   }, [errors]);
 
   if (Schedule.length === 0) {
-    return <></>;
+    return (
+      <LoadingSpinner>
+        <div className="lds-ring">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </LoadingSpinner>
+    );
   }
 
   // **** first schedule output only
